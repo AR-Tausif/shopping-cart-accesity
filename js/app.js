a11ae40c19f1bda7358b52c508ba57d3ff28deb4
AR-Tausif <tausif.ritu1@gmail.com>
2023-02-19 21:22:39 +0600

    Integred to click plush and minus button and mobile total price change

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+
+
+
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
-    console.log('object');
+    const phoneValue = getValue("phone-number-field");
+    const phoneTotalText = setInnerText('phone-total');
+    const phoneTotal = phoneTotalText * phoneValue;
+    document.getElementById('phone-total').innerText = phoneTotal;
 })
+
+
+document.getElementById('btn-phone-plus').addEventListener('click', function(){
+    const phoneValue = getValue("phone-number-field");
+    const phoneTotalText = setInnerText('phone-total');
+    const phoneTotal = phoneTotalText * phoneValue;
+    document.getElementById('phone-total').innerText = phoneTotal;
+
+})
+
+
+function inputChanging(){
+    const phoneValue = getValue("phone-number-field");
+    const phoneTotalText = setInnerText('phone-total');
+    const phoneTotal = phoneTotalText * phoneValue;
+    console.log(phoneTotal, phoneTotal);
+}
+function setInnerText(id){
+    const setextInner = document.getElementById(id);
+    const textInner= setextInner.innerText;
+    return parseFloat(textInner);
+}
+function getValue(id){
+    const htmlInput = document.getElementById(id);
+    const valueInpt = htmlInput.value;
+    return parseFloat(valueInpt);
+}
+
+
+
