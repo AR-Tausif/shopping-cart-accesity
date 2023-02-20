cb17917ece09f5dc63a48ae5782ec46c8c77735b
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2023-02-20 20:07:58 +0600

    rewrites comments for hints

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-// Calling EVENT Listener for Mobile Price Total math 
+// Calling EVENT Listener for Mobile Price Total doing math with utility function
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
     updatingDomPricesMinus("phone-number-field", "phone-total", 1219);
 })
@@ -7,7 +7,7 @@ document.getElementById('btn-phone-plus').addEventListener('click', function(){
     updatingDomPricesPlus("phone-number-field", "phone-total", 1219);
 })
 
-// Calling EVENT Listener for Mobile case Price Total math 
+// Calling EVENT Listener for Mobile case Price Total doing math with utitlity funtion
 
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
